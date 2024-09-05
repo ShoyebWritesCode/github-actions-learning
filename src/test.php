94f19c5add099f5e1b68bd71f9224d3534020219
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-09-05 16:49:48 +0600

    add test php file

--- src/test.php
+++ src/test.php
@@ -1,16 +1,45 @@
 <?php
 
 /**
- * This is a simple PHP file that prints Hello, World!
- * 
- * @category Example
- * @package  MyPackage
- * @author   Your Name <your.email@example.com>
- * @license  https://opensource.org/licenses/MIT MIT License
- * @version  PHP 8.1
- * @link     https://example.com
+ * This file contains the MyClass class.
+ *
+ * @category CI/CD
+ * @package  My\Project
+ * @author   Md. Abu Shoyeb <mdsoyeb@iut-dhaka.edu>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/ShoyebWritesCode/github-actions-learning.git
  */
 
-echo "Hello, World!";
+declare(strict_types=1);
 
-die("test");
+namespace My\Project;
+
+use My\Project\Tools;
+use My\Project\Helpers\Formatter;
+
+/**
+ * Class MyClass
+ *
+ * @category CI/CD
+ * @package  My\Project
+ * @author   Md. Abu Shoyeb <mdsoyeb@iut-dhaka.edu>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/ShoyebWritesCode/github-actions-learning.git
+ */
+class MyClass extends ParentClass implements AnInterface
+{
+    /**
+     * Processes the given arguments.
+     *
+     * @param string $arg1 First argument.
+     * @param int    $arg2 Second argument.
+     *
+     * @return void
+     */
+    public function myFunction(string $arg1, int $arg2): void
+    {
+        if ($arg1 === 'example') {
+            // Code
+        }
+    }
+}
