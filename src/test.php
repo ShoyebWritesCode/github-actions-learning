42549dac0a84697a378cddce8e9918574a8f108c
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-09-05 15:59:48 +0600

    add composer json

--- src/test.php
+++ src/test.php
@@ -1,1 +1,14 @@
-echo hello;
+<?php
+
+/**
+ * This is a simple PHP file that prints Hello, World!
+ * 
+ * @category Example
+ * @package  MyPackage
+ * @author   Your Name <your.email@example.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  PHP 8.1
+ * @link     https://example.com
+ */
+
+echo "Hello, World!";
